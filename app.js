7b3e864cad5b01ce26ff005f28a094119506edba
Jacob Wright <jacob.wright.ut@gmail.com>
2018-02-28 17:55:09 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+$(function() {
+    var current
+    var output
+    var limit
+    var zero
+    var operator
+
+    // screen = document.getElementById("result")
+    screen = $("#result")[0]
+    // var elem = document.querySelector(".num")
+    var elem = $(".num")
+    var len = elem.length
+        for(var i = 0; i < len; i++) {
+            elem[i].addEventListener("click", function(){
+                if(limit > 10) {
+                    alert("Your number is too big!")
+                } else {
+                    num = this.value
+                    output = screen.innerHTML += num
+                    limit = output.length
+                }
+                }, false)
+            }
+
+    // var elem1 = document.querySelectorAll(".operator")
+    var elem1 = $(".operator")
+    var len1 = elem1.length
+    for(var i = 0; i < len1; i++) {
+      elem1[i].addEventListener("click",function() {
+      operator = this.value
+        if(screen.innerHTML === "") {
+          screen.innerHTML = screen.innerHTML.concat("")
+        }
+        else if(output) {
+          screen.innerHTML = output.concat(operator)
+        }
+      }, false)
+    }
+
+    // document.querySelector("#clear").addEventListener("click", function() {
+    // $("#clear")[0].addEventListener("click", function() {
+    $("#clear").click(function() {    
+        screen.innerHTML = ""
+        limit = 0
+    })
+
+    // document.querySelector("#equals").addEventListener("click", function() {
+    $("#equals").click(function() {
+        if(screen.innerHTML === output) {
+            screen.innerHTML = eval(output)
+        }
+        else {
+            screen.innerHTML = ""
+        }
+    })
+
+
+    })
